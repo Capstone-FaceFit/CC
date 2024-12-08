749e8a3b61baa4186b15347474ea55faaf9112c8
ibnu fadhil <fadhilgelaws@gmail.com>
2024-12-08 19:46:59 +0700

    change the initial response message

--- backend-api/index.js
+++ backend-api/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(authRoute);
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello, World!' });
+    res.json({ message: 'Server is Running Yeay!' });
   });
 
 
